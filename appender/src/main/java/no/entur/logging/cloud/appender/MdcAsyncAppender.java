6438ee4ee36e658c80f1949846e9093b24e476f8
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2024-01-09 16:12:56 +0100

    Adjust MDC use

--- appender/src/main/java/no/entur/logging/cloud/appender/MdcAsyncAppender.java
+++ appender/src/main/java/no/entur/logging/cloud/appender/MdcAsyncAppender.java
@@ -2,6 +2,7 @@ package no.entur.logging.cloud.appender;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import org.slf4j.MDC;
+import org.slf4j.spi.MDCAdapter;
 
 import java.util.Map;
 
@@ -20,15 +21,16 @@ public class MdcAsyncAppender extends AsyncAppender {
         if (mdc.isEmpty()) {
             super.preprocess(eventObject);
         } else {
+            // make sure the original MDC values are left after this operation
+
+            Map<String, String> copyOfContextMap = MDC.getCopyOfContextMap();
             for (Map.Entry<String, String> entry : mdc.entrySet()) {
                 MDC.put(entry.getKey(), entry.getValue());
             }
             try {
                 super.preprocess(eventObject);
             } finally {
-                for (Map.Entry<String, String> entry : mdc.entrySet()) {
-                    MDC.remove(entry.getKey());
-                }
+                MDC.setContextMap(copyOfContextMap);
             }
         }
     }
