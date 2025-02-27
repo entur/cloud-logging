4a194a6f62945e3fc5182cefb9258deef0c73b49
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2025-02-27 15:15:58 +0100

    Fix on-demand HTTP status code configuration (#95)

--- on-demand/on-demand-spring-boot-starter-web/src/main/java/no/entur/logging/cloud/spring/ondemand/web/scope/HttpStatusAtLeastOrNotPredicate.java
+++ on-demand/on-demand-spring-boot-starter-web/src/main/java/no/entur/logging/cloud/spring/ondemand/web/scope/HttpStatusAtLeastOrNotPredicate.java
@@ -1,27 +1,28 @@
 package no.entur.logging.cloud.spring.ondemand.web.scope;
 
 import java.util.List;
+import java.util.function.IntPredicate;
 
 
 public class HttpStatusAtLeastOrNotPredicate extends HttpStatusAtLeastPredicate {
 
-    protected final HttpStatusNotEqualToPredicate exclude;
-    protected final HttpStatusEqualToPredicate include;
+    protected final IntPredicate exclude;
+    protected final IntPredicate include;
 
     public HttpStatusAtLeastOrNotPredicate(int limit, List<Integer> include, List<Integer> exclude) {
         super(limit);
 
-        this.exclude = new HttpStatusNotEqualToPredicate(exclude);
-        this.include = new HttpStatusEqualToPredicate(include);
+        this.include = include.isEmpty() ? (v) -> false : new HttpStatusEqualToPredicate(include);
+        this.exclude = exclude.isEmpty() ? (v) -> false : new HttpStatusNotEqualToPredicate(exclude);
     }
 
     @Override
     public boolean test(int statusCode) {
         if(super.test(statusCode)) {
-            // status code is equal or higher
+            // status code is equal or higher than limit
             return exclude.test(statusCode);
         } else {
-            // status code is lower
+            // status code is lower than limit
             return include.test(statusCode);
         }
     }
