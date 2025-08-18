7d1b2595ecd5a9338fdaff87a8b3f3342e96725c
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2025-08-18 15:48:24 +0200

    Add example for client request-response logging (#171)

---
+++ examples/gcp-web-apache-client-example/src/main/java/org/entur/example/web/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package org.entur.example.web.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf( c -> c.disable() )
+				.authorizeHttpRequests((authorize) -> {
+					authorize.anyRequest().permitAll();
+					}
+				);
+		return http.build();
+	}
+}
