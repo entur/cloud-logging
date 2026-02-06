f7bf9eb2e6947e2a8017891d02ff7b486c495974
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2026-02-06 10:08:22 +0100

    GCP timestamp foolproofing (#287)
    * GCP timestamp foolproofing
    
    * Add test

--- gcp/logback-logstash-encoder-gcp/src/main/java/no/entur/logging/cloud/gcp/logback/logstash/StackdriverTimestampJsonProvider.java
+++ gcp/logback-logstash-encoder-gcp/src/main/java/no/entur/logging/cloud/gcp/logback/logstash/StackdriverTimestampJsonProvider.java
@@ -3,7 +3,6 @@ package no.entur.logging.cloud.gcp.logback.logstash;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.fasterxml.jackson.core.JsonGenerator;
 import net.logstash.logback.composite.AbstractJsonProvider;
-import net.logstash.logback.composite.JsonWritingUtils;
 
 import java.io.IOException;
 
@@ -19,12 +18,20 @@ import java.io.IOException;
 
 public class StackdriverTimestampJsonProvider extends AbstractJsonProvider<ILoggingEvent> {
 
-	@Override
-	public void writeTo(JsonGenerator generator, ILoggingEvent event) throws IOException {
-		generator.writeObjectFieldStart("timestamp");
-		generator.writeNumberField("seconds", event.getTimeStamp() / 1000);
-		generator.writeNumberField("nanos", event.getNanoseconds());
-		generator.writeEndObject();
-	}
+    @Override
+    public void writeTo(JsonGenerator generator, ILoggingEvent event) throws IOException {
+        generator.writeObjectFieldStart("timestamp");
+
+        generator.writeNumberField("seconds", event.getTimeStamp() / 1000);
+
+        int nanoseconds = event.getNanoseconds(); // May return -1 if data unavailable.
+        if(nanoseconds >= 0) {
+            generator.writeNumberField("nanos", nanoseconds % 1000_000_000);
+        } else {
+            // get nanos from milliseconds
+            generator.writeNumberField("nanos", (event.getTimeStamp() % 1000) * 1000_000);
+        }
+        generator.writeEndObject();
+    }
 
 }
