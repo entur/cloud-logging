e84ba8f0873467b0c4af8d0c473021b757300ead
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2026-06-16 13:21:07 +0200

    Adjust tests

---
+++ stderr/stderr-micrometer-spring-boot-autoconfigure/src/test/java/no/entur/logging/cloud/spring/stderr/micrometer/TestConfiguration.java
@@ -0,0 +1,15 @@
+package no.entur.logging.cloud.spring.stderr.micrometer;
+
+import io.micrometer.core.instrument.binder.logging.LogbackMetrics;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    public LogbackMetrics logbackMetrics() {
+        return new no.entur.logging.cloud.micrometer.DevOpsLogbackMetrics();
+    }
+}
