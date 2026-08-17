bbe10ffa945af694f68a18393ed97b1abe79fe49
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2026-08-17 10:07:53 +0200

    Reuse NoOpenTelemetryAgentCondition

--- gcp/correlation-id-trace-spring-boot-gcp-grpc/src/main/java/no/entur/logging/cloud/gcp/trace/spring/grpc/GcpGrpcTraceAutoConfiguration.java
+++ gcp/correlation-id-trace-spring-boot-gcp-grpc/src/main/java/no/entur/logging/cloud/gcp/trace/spring/grpc/GcpGrpcTraceAutoConfiguration.java
@@ -1,15 +1,13 @@
 package no.entur.logging.cloud.gcp.trace.spring.grpc;
 
+import no.entur.logging.cloud.gcp.spring.NoOpenTelemetryAgentCondition;
 import no.entur.logging.cloud.gcp.trace.spring.grpc.interceptor.OrderedTraceIdGrpcMdcContextServerInterceptor;
 import no.entur.logging.cloud.trace.spring.grpc.GrpcCorrelationIdAutoConfiguration;
 import no.entur.logging.cloud.trace.spring.grpc.interceptor.OrderedCorrelationIdGrpcMdcContextServerInterceptor;
-import no.entur.logging.cloud.trace.spring.grpc.properties.GrpcMdcProperties;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
--- gcp/spring-boot-autoconfigure-gcp/src/main/java/no/entur/logging/cloud/gcp/spring/NoOpenTelemetryAgentCondition.java
+++ gcp/spring-boot-autoconfigure-gcp/src/main/java/no/entur/logging/cloud/gcp/spring/NoOpenTelemetryAgentCondition.java
@@ -1,12 +1,14 @@
-package no.entur.logging.cloud.gcp.trace.spring.grpc;
+package no.entur.logging.cloud.gcp.spring;
 
 import no.entur.logging.cloud.gcp.logback.logstash.StackdriverOpenTelemetryTraceMdcJsonProvider;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
+/**
+ * Condition that matches when the OpenTelemetry Java agent is <em>not</em> present.
+ */
 public class NoOpenTelemetryAgentCondition implements Condition {
-
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         return !StackdriverOpenTelemetryTraceMdcJsonProvider.isOtelAgent();
